1b8a87d416c42c06158faa2b44d79f7ba6699e2c
aluno <aluno@Lab-03>
2020-03-04 15:03:45 -0300

    03/03

--- convert.sh
+++ convert.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-
-
-unzip -q imagem-livros.zip
 cd imagens-livros
-mkdir imagens-livros-png
-convert *.jpg *.png
-mv *png imagens-livros-png
-zip -r imagens-livros-png.zip  imagens-livros-png/
+for imagem in *.jpg
+do 
+        echo $imagem 
+        img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
+        echo $img_sem_ext 
+        convert $imagem $img_sem_ext.png
+done
+
+cd ..
+
+
